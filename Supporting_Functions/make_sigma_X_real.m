788fd2cd51816f1ce49376d7b4f8a7c79876c48f
ahodzic7 <adish96@live.com>
2021-04-28 13:02:12 +0200

    make sigma

--- Supporting_Functions/make_sigma_X_real.m
+++ Supporting_Functions/make_sigma_X_real.m
@@ -1,13 +1,13 @@
-    h1 = [1 zeros(1,5)];
-    h2 = [zeros(1,5) 1];
+    h1 = [1 zeros(1,9)];
+    h2 = [zeros(1,9) 1];
     sigma_x = zeros(2,Hp);
     
     load('Kalman_Filter/cov_matrices.mat','measCovPipe','modelCovPipe');
-    var_D =  diag([average_dist_variance_Hp(:,time+1)]);  
-    var_model = BuildModelCovPipe4Aug(modelCovPipe); 
+    var_D = diag([average_dist_variance_Hp(:,time+1)]);  
+    var_model = blkdiag(0.001,BuildModelCovPipe4Aug(modelCovPipe),0.001); 
     var_pumps = diag([0.0142 0.0117]);
     var_measuremants = measCovPipe;
-    var_x_prev = var_measuremants;
+    var_x_prev = diag(ones(10,1)*0.0001);
     sigma_u = zeros(2,Hp);
     
     for i = 1:Hp
@@ -20,9 +20,9 @@
         var_x_prev = var_x;
     end
     
-    analyse = 0;
+    analyse = 1;
     if analyse
-        tightening = sqrt(sigma_x)*norminv(0.95);
+        tightening = sqrt(sigma_u)*norminv(0.95)*60;
         figure
         plot(tightening(1,:));
         hold on;
