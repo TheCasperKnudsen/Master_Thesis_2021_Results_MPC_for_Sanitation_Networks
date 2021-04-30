4101490fba1afcf01473272079bc6b9038abdb9a
unknown <ckn16@student.aau.dk>
2021-04-30 11:48:44 +0200

    added BuildBof

---
+++ Supporting_Functions/BuildBof_MX.m
@@ -0,0 +1,10 @@
+function Bof = BuildBof_MX(NumberOfStates,phi,DeltaT)
+%BUILDBd Returns the Bof matrix for the two tank topology
+%The first tank is included in the B matrix. dim(phi) = (1,2)
+
+Bof = casadi.MX.zeros(NumberOfStates,2);
+
+Bof(1,:) = [-phi(1) * DeltaT, 0];
+Bof(end,:) = [0, -phi(2) * DeltaT];
+end
+
